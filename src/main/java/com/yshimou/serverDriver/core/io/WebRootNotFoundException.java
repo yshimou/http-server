e3516880a058dcd3f9e6734a2d93aa9d9ded5813
yshimou <yasser.shimou@amadeus.com>
2025-03-06 07:55:18 +0100

    handling of files to serve

---
+++ src/main/java/com/yshimou/serverDriver/core/io/WebRootNotFoundException.java
@@ -0,0 +1,7 @@
+package com.yshimou.serverDriver.core.io;
+
+public class WebRootNotFoundException extends Throwable {
+  public WebRootNotFoundException(String message) {
+    super(message);
+  }
+}
